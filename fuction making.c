fe115357d0a9ae41b00b7f1bb5f9bf6a228f1105
mithuncoderai <mithunpcmithunpc@gmail.com>
2025-11-18 16:07:48 +0530

    Create fuction making.c

---
+++ fuction making.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int username(){
+    int age;
+    char name,department;
+    printf("my name is \n");
+    scanf("%s",&name);
+    printf("my age is \n");
+    scanf("%d",&age);
+    printf("my deportment is \n");
+    scanf("%s",&department);
+    }
+
+
+int main() {
+    username();{
+        
+    }
+    
+}    
+
+    
